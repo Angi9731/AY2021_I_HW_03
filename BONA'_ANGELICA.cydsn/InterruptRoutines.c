436f15ddaeb8fca15f81f14665b5269beae81e38
Angelica <vaniglia.1997@gmail.com>
2020-10-17 09:56:59 +0200

    Prima bozza

---
+++ BONA'_ANGELICA.cydsn/InterruptRoutines.c
@@ -0,0 +1,33 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "InterruptRoutines.h"
+#include "stdio.h"
+#include "project.h"
+extern unsigned int flag;
+extern uint8 count_TIMER;
+
+CY_ISR(Custom_UART_ISR)
+{
+    if(UART_ReadRxStatus() == UART_RX_STS_FIFO_NOTEMPTY)
+    {
+
+        flag = 1;
+    }
+    
+}
+
+CY_ISR(Custom_TIMER_ISR)
+{
+    count_TIMER ++;
+    Timer_ReadStatusRegister();
+}
+/* [] END OF FILE */
--- BONA'_ANGELICA.cydsn/main.c
+++ BONA'_ANGELICA.cydsn/main.c
@@ -10,16 +10,141 @@
  * ========================================
 */
 #include "project.h"
+#include "InterruptRoutines.h"
+#include "stdio.h"
+
+unsigned int flag = 0;
+uint8 count_TIMER = 0;
+
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    
+    uint8 received;
+    uint8 DC_RED = 0;
+    uint8 DC_GREEN = 0;
+    uint8 DC_BLUE = 0;
+    
+    UART_Start();
+    ISR_UART_StartEx(Custom_UART_ISR);
+    ISR_TIMER_StartEx(Custom_TIMER_ISR);
+    timer_clock_Start();
+    
+    
+    
+    uint8 start_received = 0;
+    uint8 stop_received = 1;
+    uint8 red_received = 0;
+    uint8 green_received = 0;
+    uint8 blue_received = 0;
+    
+    
+    
+    UART_PutString("Inserisci valore di Start\n");
+   
 
     for(;;)
     {
-        /* Place your application code here. */
+        if(count_TIMER >= 10)
+            {
+                
+                UART_PutString("Troppo tardi! Inserisci valore di Start\n");
+                Timer_Stop();
+                count_TIMER = 0;
+                DC_RED = 0;
+                DC_GREEN = 0;
+                DC_BLUE = 0;
+                stop_received = 1;
+            }
+        
+        if(flag == 1)
+        {
+           received = UART_ReadRxData();
+            
+            if(received == 'v')
+            {
+                UART_PutString("RGB LED Program $$$\n");
+                flag = 0;
+            }
+            
+            else if((flag) && (received == 0xA0) && (stop_received))
+            {
+               
+               UART_PutString("Inserisci valore per ROSSO\n");
+               Timer_Start();
+               count_TIMER = 0;
+               start_received ++;
+               stop_received = 0;
+               flag = 0;
+               
+            }
+            
+             else if((flag) && (start_received) && (count_TIMER < 10))
+            {
+                
+                DC_RED = received;
+                UART_PutString("Inserisci valore per VERDE\n");
+                Timer_Start();
+                count_TIMER = 0;
+                red_received ++;
+                start_received = 0;
+                flag = 0;
+                
+            }
+            
+             else if((red_received) && (flag) && (count_TIMER < 10))
+            {
+                
+                DC_GREEN = received;
+                UART_PutString("Inserisci valore per BLU\n");
+                Timer_Start();
+                count_TIMER = 0;
+                green_received ++;
+                red_received = 0;
+                flag = 0;
+               
+              
+            }
+            
+            else if((green_received) && (flag)&&(count_TIMER < 10))
+            {
+               
+               DC_BLUE = received;
+               UART_PutString("Inserisci valore di Stop\n");
+               Timer_Start();
+               count_TIMER = 0;
+               blue_received ++;
+               green_received = 0;
+               flag = 0;
+                
+            }
+            
+            else if((blue_received) && (received == 0xC0) &&(flag))
+            {
+                
+                stop_received ++;
+                blue_received = 0;
+                flag = 0;
+                
+                UART_PutChar(DC_RED);
+                
+                UART_PutChar(DC_GREEN);
+                
+                UART_PutChar(DC_BLUE);
+            }
+            
+            /*else if(((flag)&&(received != 0xA0) && (stop_received))||((blue_received) && (received != 0xC0)&&(flag)) )
+            {
+                UART_PutString("Errore\n");
+                flag = 0;
+            }*/
+            
+       
+        }   
+        
+                
+            
     }
 }
 
